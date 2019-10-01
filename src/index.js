c317832f03895d53b9d2c005a45dc00d4bfd98e9
a-popova <papova.anastasiya.13@gmail.com>
2019-10-01 12:26:44 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+
+module.exports = function getTimeForEducation(focus, knowsProgramming, config) {
+  const basicProgramming = 500;
+  const advancedProgramming = 800;
+  let weeks = 0;
+
+  if (knowsProgramming) {
+    weeks = Math.ceil(advancedProgramming / config[focus]);
+  } else {
+    weeks =  Math.ceil((basicProgramming + advancedProgramming) / config[focus]);
+  }
+
+  return weeks;
+};
